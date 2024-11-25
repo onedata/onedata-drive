8b2f460b9a89a946a7022e40aea6538ef6159ace
Andrej Gafrik <485733@mail.muni.cz>
2024-11-25 17:09:01 +0100

    NameConvertor class

---
+++ OnedataDrive/Utils/NameConvertor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace OnedataDrive.CloudSync.Utils
+{
+    internal class NameConvertor
+    {
+        const string PROHIBITED_CHARS = "<>:\"/\\|?*";
+        char[] prohibited_chars = Path.GetInvalidFileNameChars();
+        public bool WindowsCorrect(string name) 
+        {
+            return
+                name.Length > 0
+                && !name.Any(c => prohibited_chars.Contains(c))
+                && !name.EndsWith(" ");
+        }
+
+        public string MakeWindowsCorrect(string name, char replaceChar = '_')
+        {
+            if (name.Length == 0)
+            {
+                return replaceChar.ToString();
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in name)
+            {
+                if (prohibited_chars.Any(pc => pc == c))
+                {
+                    sb.Append(replaceChar);
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            if (name.EndsWith(' '))
+            {
+                sb.Remove(sb.Length, sb.Length);
+            }
+            return sb.ToString();
+        }
+    }
+}
