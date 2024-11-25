e5d9c4f66db0308b30bebda93a6bb77182743cdb
Andrej Gafrik <485733@mail.muni.cz>
2024-11-25 16:13:56 +0100

    WIP: refresh context menu - classFactory

---
+++ OnedataDrive/ClassFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanara.PInvoke;
+using Windows.ApplicationModel.Background;
+using static Vanara.PInvoke.Ole32;
+
+namespace OnedataDrive.CloudSync
+{
+    internal class ClassFactory<T> : IClassFactory
+    {
+        public HRESULT CreateInstance(object? pUnkOuter, in Guid riid, out object? ppvObject)
+        {
+            
+        }
+
+        public HRESULT LockServer(bool fLock)
+        {
+            return HRESULT.S_OK;
+        }
+    }
+}
