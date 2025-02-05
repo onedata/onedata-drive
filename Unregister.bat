e09ed53cfd87ec7d1f4480521c5436ddd5d917bd
Andrej Gafrik <485733@mail.muni.cz>
2025-02-05 12:20:32 +0100

    proof of concept context menu

---
+++ Unregister.bat
@@ -0,0 +1,41 @@
+@echo off
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorlevel% == 0 (
+    echo Running with administrative privileges
+) else (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+)
+
+goto Start
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:Start
+cd /d "%~dp0"
+
+:: Find DLL files ending with comhost.dll
+for /f "delims=" %%a in ('dir /b *comhost.dll') do set "DLLFile=%%a"
+if defined DLLFile goto Found
+
+echo No DLL file ending with comhost.dll found, please enter the DLL name:
+SET /P DLLName=Enter the DLL name to unregister (include .dll extension):
+set DLLFile=%DLLName%
+
+:Found
+if not exist "%DLLFile%" (
+    echo The specified DLL file does not exist.
+    pause
+    exit /b
+)
+
+regsvr32 /u /s "%DLLFile%"
+taskkill /f /im explorer.exe
+start explorer.exe
+echo %DLLFile% unregistered and Explorer restarted.
+pause
