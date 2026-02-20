fd3cdb62efa800052effa7b08cbaa73e57b811b5
Andrej Gafrik <gafrik@ics.muni.cz>
2026-02-20 18:55:12 +0100

    Run functio delegate in RunningTaskList async

--- OnedataDrive/PlaceholderFetcher.cs
+++ OnedataDrive/PlaceholderFetcher.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using NLog;
+using OnedataDrive.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Vanara.PInvoke.CldApi;
 
 namespace OnedataDrive
 {
-    internal class PlaceholderFetcher
+    public class PlaceholderFetcher
     {
+        private Logger logger;
+        private LoggerFormater loggerFormater;
+        public PlaceholderFetcher(Logger logger)
+        {
+            this.logger = logger;
+            this.loggerFormater = new(logger);
+        }
+
+        public void FetchPlaceholders(Callback callback)
+        {
+
+        }
     }
 }
