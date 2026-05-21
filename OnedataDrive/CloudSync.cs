2b07e35b56976c0c4c0a0dc4e743424d58420624
Andrej Gafrik <gafrik@ics.muni.cz>
2026-05-21 10:15:50 +0200

    version change

--- OnedataDrive/CloudSync.cs
+++ OnedataDrive/CloudSync.cs
@@ -10,7 +10,7 @@ namespace OnedataDrive
 {
     public static class CloudSync
     {
-        public const string VERSION = "0.5.9";
+        public const string VERSION = "25.0.0";
         public const string APP_NAME = "Onedata Drive";
         public static Logger logger = LogManager.GetCurrentClassLogger();
         public static Config configuration = new();
